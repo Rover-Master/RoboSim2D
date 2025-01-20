56e825e3dafb4e00169edd8806ead808a35bfee7
Yuxuan Zhang <github@z-yx.cc>
2025-01-20 05:08:11 -0500

    update: regenerate examples

--- README.sh
+++ README.sh
@@ -4,13 +4,16 @@ mkdir -p tmp
 run() {
 python3 -m $1.$2 data/world --prefix=tmp/ \
     --scale=2 --dpi-scale=2 \
-    --src=-3.60,-2.10 \
-    --dst=0.80,-6.57 \
+    --src=0,-6 \
+    --dst=-7.5,2 \
     > /dev/null
 mv tmp/$2.png doc/$2.png || echo "Failed to run $1.$2"
 }
 
-run simulation RandomWalk
+run simulation RandomWalk; mv doc/RandomWalk.png doc/RandomWalk-01.png
+run simulation RandomWalk; mv doc/RandomWalk.png doc/RandomWalk-02.png
+run simulation RandomWalk; mv doc/RandomWalk.png doc/RandomWalk-03.png
+run simulation RandomWalk; mv doc/RandomWalk.png doc/RandomWalk-04.png
 run simulation Bug0L
 run simulation Bug0R
 run simulation Bug1L
